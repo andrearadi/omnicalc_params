1015097a342fd362e866d14939b40e349ebd8a79
AndreaRadi <aradi@chicagobooth.edu>
2017-07-14 16:50:03 +0000

    Solved square and square root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square"})
+  
+  get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_root"})
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
